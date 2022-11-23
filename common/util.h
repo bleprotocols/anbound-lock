10fc540a8b89da1d7045b626dc2e223d523fc287
bledevices <51301213+bleprotocols@users.noreply.github.com>
2022-11-23 22:57:53 +0100

    Update util.h

--- common/util.h
+++ common/util.h
@@ -2,19 +2,19 @@
 #define _UTIL_H
 
 #include "cc254x_types.h"
+#define NOOP __asm nop __endasm;
 
 /* even if we don't use this interrupt, make sure we have a vector for it
    so that the interrupt table has at least 17 entries, otherwise SDCC makes
    a sorter table and places code just after it :s */
 void isr_wdt() __interrupt(17);
-uint16 rnd(); //Generate a 16 bits random number
+uint16 rnd(); //Generate a 16 bits random number 
 uint32 rnd32(); //Generate a 32 bits random number
 
 uint16 read_vdd(); //Read the device input voltage
 uint16 battery_soc();//read battery state of charge
 uint16 read_temperature(); //Read the chip temperature sensor
 void slow_clockspeed();//set the clockspeed to the slowest option/divider
-void pwm_motor(const uint8 pin); //pwm an output pin
-#define NOOP __asm nop __endasm;
+void pwm_motor(const uint8 pin, const uint8 pwm_on,const uint8 pwm_off);
 
 #endif
